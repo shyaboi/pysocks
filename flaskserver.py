e49de95858ce00f61c3b43822573217b3e6e7d56
Shyaboi <ian.shane.sears@gmail.com>
2020-09-27 22:29:55 -0700

    getting some movement

--- flaskserver.py
+++ flaskserver.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import random
 app = Flask(__name__)
 
 @app.route('/push', methods=['POST'])
 def receive_data():
-   print("player: " + request.form['playerNum'], "pressed"+ request.form['keyPressed']+"key")
-   return "player: " + request.form['playerNum'], "pressed"+ request.form['keyPressed']+"key"
+   print("player: " + request.form['playerNum'], "pressed the "+ request.form['keyPressed']+" key")
+   playerNum = request.form['playerNum']
+   keypressed = request.form['keyPressed']
+   rand1 = random.randint(1, 401)
+   rand2 = random.randint(1, 301)
+   x=str(rand1)
+   y=str(rand2)
+   player = jsonify(x,y)
+# f"player: { request.form['playerNum']} pressed the {request.form['keyPressed']} key"
+
+   return player
 @app.route('/')
 def hello():
     return render_template('index.html')
--- static/js.js
+++ static/js.js
@@ -2,13 +2,35 @@ const w = 119
 const a = 97
 const s = 115
 const d = 100
-let rando = Math.floor(Math.random()*1000)
+var canvas = document.getElementById("myCanvas");
+var c=document.getElementById('myCanvas');
+var ctx=c.getContext('2d');
+ctx.translate(100,0);
+ctx.fillRect(0,0,100,50);
+
+let rando = Math.floor(Math.random()*400)
 console.log(rando)
+
 $(document).keypress(function(e) {
   let keyPressed = e.keyCode
   // console.log(keyPressed)
   $.post("/push", {'keyPressed':keyPressed, 'playerNum':rando },(data)=> {
-  console.log(data)
+    let y = data[1]
+    let x = data[0]
+    console.log(x,y)
+    if (keyPressed == d) {
+      ctx.translate(10,0)
+      ctx.fillRect(0,0,100,50);
+  
+    }
+    if (keyPressed == s) {
+      ctx.translate(0,10)
+      ctx.fillRect(0,0,100,50);
+
+    }
+
   });
   
 });
+
+
