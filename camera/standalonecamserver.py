6b0ae7218b407ee5302ae33b8f99a1afc8d65aa6
Shyaboi <ian.shane.sears@gmail.com>
2020-10-03 12:06:22 -0700

    movign js

--- camera/standalonecamserver.py
+++ camera/standalonecamserver.py
@@ -40,8 +40,8 @@ PAGE="""\
 
 <center><h1>Raspberry Pi - Surveillance Camera</h1></center>
 <center><img src="stream.mjpg" width="640" height="480"></center>
-</body>
 <script src="../static/js.js"></script>
+</body>
 
 </html>
 """
