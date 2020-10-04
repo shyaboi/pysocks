25014ae0861d0073c7197540b6d371a45864c862
Shyaboi <ian.shane.sears@gmail.com>
2020-10-03 23:17:09 -0700

    set potato fps

--- camera/standalonecamserver.py
+++ camera/standalonecamserver.py
@@ -186,7 +186,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='400x250', framerate=19) as camera:
+with picamera.PiCamera(resolution='400x250', framerate=5) as camera:
     output = StreamingOutput()
     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
